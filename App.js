bae64315678852b00e3108fc8f750652ad64cd22
Justinnn07 <justinv2006@gmail.com>
2021-02-07 21:27:25 +0530

    initial commit :fire:

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <img
+        src="https://i.ytimg.com/an_webp/UBBHpoW3AKA/mqdefault_6s.webp?du=3000&sqp=CMDew4AG&rs=AOn4CLCWO6WT8OCC1jcxa_CAoO_vyHn83Q"
+        alt=""
+        style={{ margin: "10px" }}
+      />
+      <input style={{ outlineWidth: 0 }} type="text" placeholder="input.." />
+      <StatusBar style="dark" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    flex: "1",
   },
 });
